38a6d95c619b722741ef02026f3b6ef6546bbb79
Just_Sample <just.sample@mail.ru>
2019-11-12 14:45:56 +0700

    Создал скрипт который отвечает за рисовку и обновление кадров игры. Так же добавил состояние игры и добавил скрипт в html

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let ctx = canvas.getContext('2d');
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-
+let game = new Game(GAME_WIDTH,GAME_HEIGHT);
 
 let lastTime = 0;
 
@@ -16,8 +16,10 @@ function gameLoop(timeStamp)
     lastTime = timeStamp;
 
     ctx.clearRect(0,0,GAME_WIDTH,GAME_HEIGHT);
-    
 
+    game.update(deltaTime);
+    game.draw(ctx);
+    
     requestAnimationFrame(gameLoop);
 }
 
