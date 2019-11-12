56b8003fc8a2599a3a80a50515439cf858e04123
Just_Sample <just.sample@mail.ru>
2019-11-12 14:46:47 +0700

    да, добавил....

---
+++ js/game.js
@@ -0,0 +1,50 @@
+const GAME_STATE = {
+    PAUSED: 0,
+    RUNNING: 1,
+    MENU: 2,
+    GAME_OVER:3,
+}
+
+class Game{
+
+    constructor(gameWidth, gameHeight){
+        
+        //Расширение canvas
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+
+        this.gameObjects = [];
+
+        //Состояние игры
+        this.gameState = GAME_STATE.MENU;
+        this.HP = 100;
+        this.score = 0;
+        //Нажатие клавишь
+
+    }
+
+    start()
+    {
+        this.gameObjects =[
+
+
+
+        ]
+    }
+
+    update(deltaTime)
+    {
+        this.gameObjects.forEach((object) => object.update(deltaTime));
+    }
+
+    draw(ctx)
+    {
+        this.gameObjects.forEach((object) => object.draw(ctx));
+    }
+
+    togglePause()
+    {
+
+    }
+
+}
