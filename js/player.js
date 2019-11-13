0148e5f3e060b079a34e0ccc3483929939900d41
Just_Sample <just.sample@mail.ru>
2019-11-13 15:43:56 +0700

    Небольшой рефакторинг. Добавил комментарии и отредактировал состояние игрока

--- js/player.js
+++ js/player.js
@@ -4,15 +4,17 @@ const PLAYER_STATE = {
     IdleLeft: 1,
     RunRight: 2,
     RunLeft: 3,
-    JumpRight: 4,
-    JumpLeft: 5,
-    Dead: 6
+    JumpRightUp: 4,
+    JumpRightDown: 5,
+    JumpLeftUp: 6,
+    JumpLeftDown: 7
 }
 
 class Player{
 
     constructor(game){
 
+        //Animations
         this.image = document.getElementById("img_TimonIdle");
         this.imageIdleLeft = document.getElementById("img_TimonIdleLeft");
         this.imageRunRight = document.getElementById("img_TimonRunRight");
@@ -25,19 +27,20 @@ class Player{
             this.imageRunLeft
         ];
 
-        console.log(this.Animations[2].width);
-
         this.playerState = PLAYER_STATE.IdleRight;
-
+        
+        //fix me
         this.width = this.Animations[this.playerState].width;
         this.height = this.Animations[this.playerState].height;
 
+        //For physics
         this.dx = 0;
         this.dy = 0;
 
         this.maxSpeedPhysics = 10;
         this.speedPhysics = 0.3;
 
+        //Контроль анимации
         this.frameIndex = 0; //Номер фрейма
         this.tickCount = 0; //Кол-во обновлений прошедшие с первого выхода
         this.ticksPerFrame = 10 || 0; //Кол-во обновлений должно пройти
@@ -79,28 +82,30 @@ class Player{
         if(this.playerState === PLAYER_STATE.IdleRight || this.playerState === PLAYER_STATE.IdleLeft){
             
             this.numberOfFrames = 4;
-        }
 
-        if(this.playerState === PLAYER_STATE.RunRight || this.playerState === PLAYER_STATE.RunLeft){
+        }else if(this.playerState === PLAYER_STATE.RunRight || this.playerState === PLAYER_STATE.RunLeft){
             
             this.numberOfFrames = 9;
+
         }
 
+        //Отрисовка кадров
         ctx.drawImage(
-            this.Animations[this.playerState],
-            this.frameIndex * this.Animations[this.playerState].width / this.numberOfFrames,
+            this.Animations[this.playerState], //Какой именно изображение 
+            this.frameIndex * this.Animations[this.playerState].width / this.numberOfFrames,//Показывает нужный нам фрейм
             0,
             this.Animations[this.playerState].width / this.numberOfFrames,
             this.Animations[this.playerState].height,
-            this.position.x,
-            this.position.y,
-            this.Animations[this.playerState].width / this.numberOfFrames,
-            this.Animations[this.playerState].height
+            this.position.x,//Позиция по x
+            this.position.y,//Позиция по y
+            this.Animations[this.playerState].width / this.numberOfFrames,//Корректно отображает нужный нам кадр по ширине
+            this.Animations[this.playerState].height//Корректно отображает нужный нам кадр по высоте
         )
     }
 
     update(deltaTime){
 
+        //Через какое время нужно показывать новый фрейм
         this.tickCount++;
  
         if (this.tickCount > this.ticksPerFrame) {
@@ -112,6 +117,8 @@ class Player{
             }
         }
 
+
+        //Чтобы игрок не вышел за зону игры
         this.position.x += this.speed;
 
         if(this.position.y + this.Animations[this.playerState].height >= this.gameHeight) this.position.y = this.gameHeight - this.Animations[this.playerState].height;
