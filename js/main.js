aabe98a2bd8ebdec08c32405b9c3f9a13b4ec1c3
Just_Sample <just.sample@mail.ru>
2019-11-17 13:04:06 +0700

    Сделал начало игры по нажатию кнопки + стартовая страница игры + поменял font на странице.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
+let btn = document.getElementById("this-btn_start");
+let hide = document.getElementsByClassName("info");
+//console.log(hide);
+
+function BeginPlay() {
+
+    for(let i = 0;i != hide.length;){
+        console.log(hide);
+        hide[i].parentNode.removeChild(hide[i]);
+    }
+
+let cns = document.createElement('canvas');
+cns.id = "gameScreen";
+document.body.appendChild(cns);
+
 let canvas = document.getElementById("gameScreen");
 
 let ctx = canvas.getContext('2d');
 
 const GAME_WIDTH = 1280;
 const GAME_HEIGHT = 720;
+
+canvas.width = 1280;
+canvas.height = 720;
 
 let game = new Game(GAME_WIDTH,GAME_HEIGHT);
 
@@ -25,3 +43,7 @@ function gameLoop(timeStamp)
 }
 
 requestAnimationFrame(gameLoop);
+
+}
+
+btn.onclick = BeginPlay;
